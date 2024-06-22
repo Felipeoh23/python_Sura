c5ccf1a6a3f38de4ba90e8fbd9ececc130265077
Felipeoh23 <felipeolayahernandez@gmail.com>
2024-06-21 20:28:03 -0500

    update

---
+++ EstructuraList.py
@@ -0,0 +1,15 @@
+
+my_list = ["Negro", "Rojo", "Azul", "Verde"]
+
+print(my_list[1])
+my_list[0] = "Amarillo"
+print(my_list[0])
+
+my_list.append("Rosado")
+print(my_list)
+
+numbers = [1,2,3,4,5,6,7,8,9]
+
+print(numbers[4:8])
+
+print(numbers[1:-6])

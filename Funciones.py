f4e2b0e484af889aced9f2515b9a1a7fc160c8b8
Felipeoh23 <felipeolayahernandez@gmail.com>
2024-06-28 19:38:52 -0500

    update

--- Funciones.py
+++ Funciones.py
@@ -9,6 +9,7 @@ def calculateNetSalary(salary):
 
  return netSalary
  """
+"""
 def calculate_net_salary(salary, health_discount_rate, pension_discount_rate):
     health_discount = salary * health_discount_rate
     pension_discount = salary * pension_discount_rate
@@ -25,4 +26,26 @@ pension_discount_rate = float(input("Ingrese la tasa de descuento de pensión (0
 net_salary = calculate_net_salary(salary, health_discount_rate, pension_discount_rate)
 
 print("El salario neto es:", net_salary)
+"""
+def calculate_net_salary(salary, health_discount_rate, pension_discount_rate, min_legal_salary):
+    health_discount = salary * health_discount_rate
+    pension_discount = salary * pension_discount_rate
 
+    transportation_aid = 0
+    if salary <= 2 * min_legal_salary:
+        transportation_aid = 106454 
+
+    net_salary = salary - health_discount - pension_discount + transportation_aid
+
+    return net_salary
+
+min_legal_salary = 1300000 
+
+
+salary = int(input("Ingrese el salario: "))
+health_discount_rate = float(input("Ingrese la tasa de descuento de salud (0.04 para 4%): "))
+pension_discount_rate = float(input("Ingrese la tasa de descuento de pensión (0.04 para 4%): "))
+
+net_salary = calculate_net_salary(salary, health_discount_rate, pension_discount_rate, min_legal_salary)
+
+print("El salario neto es:", net_salary)
